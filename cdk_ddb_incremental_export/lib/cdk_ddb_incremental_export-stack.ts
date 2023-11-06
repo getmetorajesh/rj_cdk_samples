ed9e4c39f7f8f5fa736e24896cca80d4290791ca
raj <getmetorajesh@gmail.com>
2023-11-07 09:57:40 +1100

    feat: CDK dynamodb backup

---
+++ cdk_ddb_incremental_export/lib/cdk_ddb_incremental_export-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from "aws-cdk-lib";
+import { AttributeType, StreamViewType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Rule, Schedule } from "aws-cdk-lib/aws-events";
+import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import path = require("path");
+
+export class CdkDdbIncrementalExportStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const backupBucket = new Bucket(this, "ExportBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+    });
+
+    const tableToBeBacked = new Table(this, "ExportTable", {
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pointInTimeRecovery: true,
+      stream: StreamViewType.NEW_IMAGE,
+    });
+
+    const exportFunction = new Function(this, "ExportFunction", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: Code.fromAsset(path.resolve(__dirname, "./scripts/export_to_s3")),
+      environment: {
+        TABLE_NAME: tableToBeBacked.tableName,
+        BUCKET_NAME: backupBucket.bucketName,
+      },
+    });
+
+    const exportRule = new Rule(this, "ExportRule", {
+      schedule: Schedule.cron({ hour: "*", minute: "30" }),
+      targets: [new LambdaFunction(exportFunction)],
+    });
+
+    tableToBeBacked.grantStreamRead(exportFunction);
+    backupBucket.grantReadWrite(exportFunction);
+  }
+}
